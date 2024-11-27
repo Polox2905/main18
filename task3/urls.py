7126ac3f95aa2bf4ad8834feb0807e8a7c48cf75
Polox2905 <pauitr1984@gmail.com>
2024-11-27 21:27:17 +0300

    Fourth commit

--- task3/urls.py
+++ task3/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import platform_view, games_view, cart_view
+from .views import platform_view, games_view, cart_view, add_to_cart
 
 urlpatterns = [
     path('', platform_view, name='platform'),
-    path('games/', games_view, name='games'),
+    path('store/', games_view, name='games'),
     path('cart/', cart_view, name='cart'),
+    path('add-to-cart/<slug:item_id>/', add_to_cart, name='add_to_cart'),
 ]
--- task3/views.py
+++ task3/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
 # Create your views here.
 def platform_view(request):
@@ -14,6 +14,34 @@ def games_view(request):
     context = {'items': items}
     return render(request, 'third_task/games.html', context)
 
+
 def cart_view(request):
-    context = {}
+    cart = request.session.get('cart', {})
+
+    items = {
+        'item1': {'name': 'Atomic heart', 'price': '$10'},
+        'item2': {'name': 'CyberPunk 2000', 'price': '$20'},
+        'item3': {'name': 'PayDay2', 'price': '$30'}
+    }
+
+    cart_items = {key: {'name': items[key]['name'], 'qty': value} for key, value in cart.items()}
+    total_price = sum(int(items[key]['price'][1:]) * qty for key, qty in cart.items() if key in items)
+
+    context = {
+        'cart_items': cart_items,
+        'items': items,
+        'total_price': total_price
+    }
+
     return render(request, 'third_task/cart.html', context)
+
+
+def add_to_cart(request, item_id):
+    cart = request.session.get('cart', {})
+
+    cart[item_id] = cart.get(item_id, 0) + 1
+
+    request.session['cart'] = cart
+
+    return redirect('games')
+
